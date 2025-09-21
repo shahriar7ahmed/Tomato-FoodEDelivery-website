e697d9678a945c181fd862f6bd358c28817f3b34
Shahriar Ahmed <chatpgt7@gmail.com>
2025-09-21 12:26:23 +0600

    added home page navbar is complete and also creates some components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
-import Navbar from './components/Navbar/Navbar.jsx'
+import React from "react";
+import Navbar from "./components/Navbar/Navbar.jsx";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home/Home.jsx";
+import Cart from "./pages/Cart/Cart.jsx";
+import PlaceOrder from "./pages/PlaceOrder/PlaceOrder.jsx";
+
 const App = () => {
   return (
-    <div className='App'>
+    <div className="App">
       <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/place-order" element={<PlaceOrder />} />
+      </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,23 +1,43 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 import "./Navbar.css";
-import { assets } from "../../assets/assets"; 
+import { assets } from "../../assets/assets";
 
 const Navbar = () => {
-    const [menu,setMenu] = useState("home");
+  const [menu, setMenu] = useState("home");
   return (
     <div className="Navbar">
       <img src={assets.logo} alt="Logo" className="logo" />
       <ul className="navbar-menu">
-        <li className={menu === "home" ? "active" : ""}>Home</li>
-        <li className={menu === "about" ? "active" : ""}>About</li>
-        <li className={menu === "mobile-app" ? "active" : ""}>Mobile App</li>
-        <li className={menu === "contact-us" ? "active" : ""}>Contact Us</li>
+        <li
+          onClick={() => setMenu("home")}
+          className={menu === "home" ? "active" : ""}
+        >
+          Home
+        </li>
+        <li
+          onClick={() => setMenu("about")}
+          className={menu === "about" ? "active" : ""}
+        >
+          About
+        </li>
+        <li
+          onClick={() => setMenu("mobile-app")}
+          className={menu === "mobile-app" ? "active" : ""}
+        >
+          Mobile App
+        </li>
+        <li
+          onClick={() => setMenu("contact-us")}
+          className={menu === "contact-us" ? "active" : ""}
+        >
+          Contact Us
+        </li>
       </ul>
       <div className="navbar-right">
         <img src={assets.search_icon} alt="Search" />
         <div className="navbar-search-icon">
-            <img src={assets.basket_icon} alt="User" />
-            <div className="dot"></div>
+          <img src={assets.basket_icon} alt="User" />
+          <div className="dot"></div>
         </div>
         <button>Sign In</button>
       </div>
